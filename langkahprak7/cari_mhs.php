3d641932ad69d2129886c5590fafa54c9fcf627a
Maramar520 <75872441+Maramar520@users.noreply.github.com>
2021-12-07 23:24:09 +0700

    Add files via upload

--- langkahprak7/cari_mhs.php
+++ langkahprak7/cari_mhs.php
@@ -16,12 +16,18 @@ echo "<b>Hasil pencarian : ".$cari."</b>";
 <table border="1">
 <tr>
 <th>No</th>
+<th>NIM</th>
 <th>Nama</th>
+<th>jkel</th>
+<th>alamat</th>
+<th>tgllhr</th>
+<th>email</th>
 </tr>
+<a href="lap_mhs.php" target="_BLANK">PRINT</a>
 <?php 
 if(isset($_GET['cari'])){
 $cari = $_GET['cari'];
-$sql="select * from mahasiswa where nama like'%".$cari."%'";
+$sql="select * from mahasiswa where nim like'%".$cari."%'";
 $tampil = mysqli_query($con,$sql);
 }else{
 $sql="select * from mahasiswa";
@@ -30,9 +36,15 @@ $tampil = mysqli_query($con,$sql);
 $no = 1;
 while($r = mysqli_fetch_array($tampil)){
 ?>
+
 <tr>
 <td><?php echo $no++; ?></td>
+<td><?php echo $r['nim']; ?></td>
 <td><?php echo $r['nama']; ?></td>
+<td><?php echo $r['jkel']; ?></td>
+<td><?php echo $r['alamat']; ?></td>
+<td><?php echo $r['tgllhr']; ?></td>
+<td><?php echo $r['email']; ?></td>
 </tr>
 <?php } ?>
 </table>
